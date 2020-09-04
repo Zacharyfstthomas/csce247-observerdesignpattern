6fb09911b301c50f99c5a4d8741360c1fb79006f
Zachary St. Thomas <zfs@email.sc.edu>
2020-09-04 17:59:42 -0400

    Update Observer.java

--- ObserverDesignPattern/src/observerdesignpattern/Observer.java
+++ ObserverDesignPattern/src/observerdesignpattern/Observer.java
@@ -1,7 +1,5 @@
 package observerdesignpattern;
 /**
- * 
- * 
  * A template for Observers, to ensure it is informed when the subject sends out a new
  * status or anything else that warrents sending out, and maintains a log of all statuses
  * previously sent out by the Subject.
